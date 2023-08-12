08cffa521da8263feac9723b58f3ac8d3d46b1e1
Azim <azimuddin807@gmail.com>
2023-08-12 21:33:51 +0600

    liense added

--- wp-todo.php
+++ wp-todo.php
@@ -1,17 +1,17 @@
 <?php
 /*
  * Plugin Name:       WP Todo
- * Plugin URI:        https://example.com/plugins/the-basics/
- * Description:       Handle the basics with this plugin.
- * Version:           1.10.3
+ * Plugin URI:        https://github.com/mdazim12/wp-todo-plugin
+ * Description:       This plugin is developed for practic perpose.
+ * Version:           1.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            John Smith
- * Author URI:        https://author.example.com/
+ * Author:            Md Azim Uddin
+ * Author URI:        https://github.com/mdazim12
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI:        https://example.com/my-plugin/
- * Text Domain:       my-basics-plugin
+ * Text Domain:       wpdoto
  * Domain Path:       /languages
  */
 
